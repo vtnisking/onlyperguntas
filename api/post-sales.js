6f45e9e7b2bb14e75faea8f41e562c1506d88c0f
Vitor Manoel <vtnisking@hotmail.com>
2026-07-30 15:53:13 -0300

    Debug headers

--- api/post-sales.js
+++ api/post-sales.js
@@ -370,6 +370,9 @@ function buildCounts(cases) {
 export default async function handler(req, res) {
   res.setHeader("Cache-Control", "no-store");
 
+  console.log("HEADERS RECEBIDOS:");
+console.log(req.headers);
+
   try {
     if (req.method !== "GET") {
       res.setHeader("Allow", "GET");
